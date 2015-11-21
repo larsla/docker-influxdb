1630c2951b8dd0962f953734a4135e758eb54df1
Lars Larsson <lars.la@gmail.com>
2015-11-21 16:30:18 +0100

    fixed name for build container

--- build.sh
+++ build.sh
@@ -6,11 +6,10 @@ ARCH=${1:-x86_64}
 
 docker build -t influxdb-build -f Dockerfile-${ARCH}.build .
 
-CID=`docker run influxdb-build /output`
+docker rm influxdb-build || true
+docker run --name influxdb-build influxdb-build /output
 
 [ -d output ] || mkdir output
-docker cp $CID:/output/influxd output
-
-docker rm $CID
+docker cp influxdb-build:/output/influxd output
 
 docker build -t larsla/influxdb-${ARCH} -f Dockerfile-${ARCH} .
